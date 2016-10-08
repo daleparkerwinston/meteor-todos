35b993a5e763ac68d5234dee08c6c49466864724
Dale Winston <dale.winston@aptecllc.com>
2016-10-08 12:02:51 -0400

    test

--- imports/components/todoList/todoList.js
+++ imports/components/todoList/todoList.js
@@ -1,7 +1,7 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import { Tasks } from '../../api/tasks.js'
-import template from './todoList.html';
+//import template from './todoList.html';
 
 class TodoListCtrl {
     constructor($scope) {
